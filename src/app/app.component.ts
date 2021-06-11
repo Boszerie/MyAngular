0ffce49fb0630b7f54bd170613bac1897a1e8ef7
Bossravisut <rs.ravisut@gmail.com>
2021-06-11 16:58:43 +0700

    initail commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular';
+  ninjaname = 'UchihaBoss';
+  ninjaConsole(){
+    console.log('My name is :' ,this.ninjaname);
+  }
+  changeNinjaName(name:string){
+    this.ninjaname = name;
+  }
+  value2: string ='Hello PrimeNG';
+  
+  cars = [
+    {
+      year:1998,
+      brand:'Boszerie'
+    }
+  ];
+
+  employeeCode = '007';
+  employeeName = 'BoppinCode';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InputTextModule} from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    FormsModule,
+    TableModule,
+    ButtonModule,
+    MessagesModule,
+    MessageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
